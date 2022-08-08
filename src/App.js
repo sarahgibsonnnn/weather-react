312f1fec64dc1b04e1b28bb1499332c5ad17387a
sarahgibsonnnn <sarah_gibson2@hotmail.co.uk>
2022-08-08 21:01:18 +0100

    Added GitHub Repository Link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
     <div className="App">
       <h1>Weather Search Engine</h1>
       <Search />
+      <div className="repository-link">
+        
+       <a href='https://github.com/sarahgibsonnnn/weather-react' target="_blank" rel="noopener noreferrer" title="Open GitHub Repository" >
+        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub Logo" />
+        </a>
+      </div>
+    
     </div>
   );
 }
